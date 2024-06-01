9aa28d1c01738618de72c61776309d7ca2d8dd5d
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2024-06-01 11:47:01 +0900

    住所と病院名をリストでとってこれるようになった*

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // STEP 1
 // ページ上で、特定のクラス名の要素を取ってくる
 const table = document.querySelector('.tbl_search_roster');
+// <tbody>
+const tableBody = table.firstElementChild;
+
+const hospitalList = [];
+for (const row of Array.from(tableBody.children)) {
+
+    if (row.children.item(7) && row.children.item(7).textContent.includes('東京都')) {
+        // ここにコード追加
+        // console.log(row.children.item(9).textContent);
+        hospitalList.push({
+            'hostpital': row.children.item(9).textContent,
+            'address': row.children.item(7).textContent
+        });
+    }
+    
+    
+}
